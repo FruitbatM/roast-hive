a32bd4cf0675a9033b34a918d3f6c11a9c7a5b56
marinaP <mar.pavlovic@gmail.com>
2023-05-27 18:21:36 +0200

    Add scroll back to top functionality

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -12,3 +12,23 @@ $(document).ready(function () {
         }
     });
 });
+
+// Back to Top Arrow - code with modification taken from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+mybutton = document.getElementById('arrow_2top');
+
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200|| document.documentElement.scrollTop > 200) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the page
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
